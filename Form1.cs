cf881ffb7eab32e1e8a1db13390b2ced1dfafac7
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-14 13:00:33 +0100

    small verify log update

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +20,22 @@ namespace WebUrlChecker
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string url = ce_ComboBox1.SelectedItem + txtBox1.Text + ce_ComboBox2.SelectedItem;
 
+            if (txtBox1.Text == string.Empty)
+            {
+                richTextBox1.Text = "please fill in the textbox for looking if a url is valid / in use or not";
+            }
+
+            else
+            {
+                richTextBox1.Text = IsValidURL(url).ToString();
+
+                if (richTextBox1.Text == "True")
+                {
+                    richTextBox1.Text = txtBox1.Text + " - " + IsValidURL(url).ToString();
+                }
+            }
         }
 
         // Check if the url is valid
