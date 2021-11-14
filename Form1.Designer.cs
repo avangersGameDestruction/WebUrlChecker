5937825e7e4300ca21ca6a557fbf4c0a158f7373
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-14 13:52:28 +0100

    add full url option

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,57 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ce_ComboBox1 = new Ce_ComboBox();
-            this.txtBox1 = new TxtBox();
-            this.ce_ComboBox2 = new Ce_ComboBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.ce_ComboBox2 = new Ce_ComboBox();
+            this.txtBox1 = new TxtBox();
+            this.ce_ComboBox1 = new Ce_ComboBox();
+            this.txtBox2 = new TxtBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // ce_ComboBox1
-            // 
-            this.ce_ComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.ce_ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.ce_ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.ce_ComboBox1.FormattingEnabled = true;
-            this.ce_ComboBox1.ItemHeight = 18;
-            this.ce_ComboBox1.Items.AddRange(new object[] {
-            "http://",
-            "https://"});
-            this.ce_ComboBox1.Location = new System.Drawing.Point(12, 12);
-            this.ce_ComboBox1.Name = "ce_ComboBox1";
-            this.ce_ComboBox1.Size = new System.Drawing.Size(121, 24);
-            this.ce_ComboBox1.TabIndex = 0;
-            // 
-            // txtBox1
-            // 
-            this.txtBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.txtBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.txtBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtBox1.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold);
-            this.txtBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.txtBox1.Location = new System.Drawing.Point(139, 13);
-            this.txtBox1.Name = "txtBox1";
-            this.txtBox1.Size = new System.Drawing.Size(195, 23);
-            this.txtBox1.TabIndex = 1;
-            this.txtBox1.Text = "enter a name here";
-            // 
-            // ce_ComboBox2
-            // 
-            this.ce_ComboBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.ce_ComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.ce_ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.ce_ComboBox2.FormattingEnabled = true;
-            this.ce_ComboBox2.ItemHeight = 18;
-            this.ce_ComboBox2.Items.AddRange(new object[] {
-            ".com",
-            ".net",
-            ".org",
-            "more added soon"});
-            this.ce_ComboBox2.Location = new System.Drawing.Point(340, 13);
-            this.ce_ComboBox2.Name = "ce_ComboBox2";
-            this.ce_ComboBox2.Size = new System.Drawing.Size(133, 24);
-            this.ce_ComboBox2.TabIndex = 2;
             // 
             // richTextBox1
             // 
@@ -104,12 +61,86 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // ce_ComboBox2
+            // 
+            this.ce_ComboBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ce_ComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ce_ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ce_ComboBox2.FormattingEnabled = true;
+            this.ce_ComboBox2.ItemHeight = 18;
+            this.ce_ComboBox2.Items.AddRange(new object[] {
+            ".com",
+            ".net",
+            ".org",
+            "more added soon"});
+            this.ce_ComboBox2.Location = new System.Drawing.Point(340, 13);
+            this.ce_ComboBox2.Name = "ce_ComboBox2";
+            this.ce_ComboBox2.Size = new System.Drawing.Size(133, 24);
+            this.ce_ComboBox2.TabIndex = 2;
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.txtBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox1.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold);
+            this.txtBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtBox1.Location = new System.Drawing.Point(139, 13);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(195, 23);
+            this.txtBox1.TabIndex = 1;
+            this.txtBox1.Text = "enter a name here";
+            // 
+            // ce_ComboBox1
+            // 
+            this.ce_ComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ce_ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ce_ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ce_ComboBox1.FormattingEnabled = true;
+            this.ce_ComboBox1.ItemHeight = 18;
+            this.ce_ComboBox1.Items.AddRange(new object[] {
+            "http://",
+            "https://"});
+            this.ce_ComboBox1.Location = new System.Drawing.Point(12, 12);
+            this.ce_ComboBox1.Name = "ce_ComboBox1";
+            this.ce_ComboBox1.Size = new System.Drawing.Size(121, 24);
+            this.ce_ComboBox1.TabIndex = 0;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.txtBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox2.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold);
+            this.txtBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtBox2.Location = new System.Drawing.Point(12, 238);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(322, 34);
+            this.txtBox2.TabIndex = 5;
+            this.txtBox2.Text = "enter full name here";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(340, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "check full url";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(485, 244);
+            this.ClientSize = new System.Drawing.Size(485, 284);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.ce_ComboBox2);
@@ -118,6 +149,8 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,6 +163,8 @@
         private Ce_ComboBox ce_ComboBox2;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
+        private TxtBox txtBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,16 @@ namespace WebUrlChecker
 {
     public partial class Form1 : Form
     {
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
         public Form1()
         {
             InitializeComponent();
@@ -33,7 +44,7 @@ namespace WebUrlChecker
 
                 if (richTextBox1.Text == "True")
                 {
-                    richTextBox1.Text = txtBox1.Text + " - " + IsValidURL(url).ToString();
+                    richTextBox1.Text = txtBox1.Text + " - " + IsValidAndReachable(url).ToString();
                 }
             }
         }
@@ -44,6 +55,73 @@ namespace WebUrlChecker
             Uri uriResult;
             return Uri.TryCreate(url, UriKind.Absolute, out uriResult)
                 && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        private bool IsValidAndReachable(string url)
+        {
+            if (IsValidURL(url))
+            {
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.Timeout = 5000;
+                    request.Method = "HEAD";
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("true means its already in use false mean its not in use on the internet created by avangers MIT license", "Web Url Checker", MessageBoxButtons.OK, (MessageBoxIcon)MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fullurl = txtBox2.Text;
+
+            if (txtBox2.Text == string.Empty)
+            {
+                richTextBox1.Text = "please fill in the textbox for looking if a url is valid / in use or not";
+            }
+
+            else
+            {
+                richTextBox1.Text = IsValidURL(fullurl).ToString();
+
+                if (richTextBox1.Text == "True")
+                {
+                    richTextBox1.Text = txtBox2.Text + " - " + IsValidAndReachable(fullurl).ToString();
+                }
+
+                else if(richTextBox1.Text == "False")
+                {
+                    richTextBox1.Text = txtBox2.Text + " - " + IsValidAndReachable(fullurl).ToString();
+                }
+
+                else
+                {
+                    richTextBox1.Text = "something did go wrong please report the isseu or try again";
+                }
+            }
         }
     }
 }
